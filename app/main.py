147cc54b04fb83cdb94afb777a5b3adf62737620
Marcel Cattaneo <marcel.cattaneo@zpesystems.com>
2022-04-04 12:17:34 -0300

    adding requirements option, all licenses option

--- app/main.py
+++ app/main.py
@@ -11,17 +11,20 @@ from configparser import ConfigParser
 from typing import Tuple, Dict, List, Optional, Union
 
 config = ConfigParser()
+CONTEXT_SETTINGS = dict(help_option_names=['-h', '--help'])
 
-@click.command()
+@click.command(context_settings=CONTEXT_SETTINGS)
 @click.option('-c', '--check', is_flag=True, type=bool, help='Check licenses from the requirements.txt file.')
 @click.option('-b', '--blocked', is_flag=True, default=False, type=bool, help='Print blocked list.')
 @click.option('-p', '--permitted', is_flag=True, default=False, type=bool, help='Print permitted list.')
 @click.option('-q', '--quiet', is_flag=True, default=False, type=bool, help='Do not print any output.')
 @click.option('-v', '--verbose', is_flag=True, default=False, type=bool, help='Print a detailed output for blocked packages.')
+@click.option('-r', 'requirements', default="requirements.txt", type=str, help='Indicate the requirements file to be used.')
+@click.option('-A', '--all', 'all_requirements', is_flag=True, default=False, type=str, help='Print all available licenses based on the requirements file.')
 @click.pass_context
-def cli(ctx, check, blocked, permitted, quiet, verbose):
+def cli(ctx, check, blocked, permitted, quiet, verbose, requirements, all_requirements):
     """
-    Main command related to the license scope.
+    Tool that checks if all licenses from a project requirements are complient with FOSS.
     """
     config_path = os.path.join(os.path.dirname(os.path.abspath(__file__)), 'config/default.ini')
     config.read(config_path)
@@ -43,8 +46,13 @@ def cli(ctx, check, blocked, permitted, quiet, verbose):
             click.echo(f' - {i}')
         sys.exit(0)
 
-    package_details = get_package_list_from_requirements()
+    package_details = get_package_list_from_requirements(requirements)
+    if all_requirements:
+        print(json.dumps(package_details, indent=2))
+        sys.exit(0)
     blocked_list = check_blocked_licenses(package_details, blocked_licenses_list, verbose)
+
+    print()
 
 
     if len(blocked_list) > 0:
@@ -83,13 +91,13 @@ def check_blocked_licenses(packages_details_list, blocked_licenses_list, verbose
     return blocked_list
 
 
-def get_package_list_from_requirements() -> List:
-    if not exists("requirements.txt"):
-        click.echo("Did not found a 'requirements.txt' file.")
+def get_package_list_from_requirements(requirements: str = "requirements.txt") -> List:
+    if not exists(requirements):
+        click.echo(f"Did not found a '{requirements}' file.")
         sys.exit(1)
 
     packages_list = list()
-    with open(file='requirements.txt') as file:
+    with open(file=requirements) as file:
         for line in file:
             package = line.split('==')[0]
             if package[0] != "-":
